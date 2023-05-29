a20f448384c295c8cd2a2fe99233cba7735b0381
SavioSantosDev <saviogta1@gmail.com>
2023-05-29 20:48:48 -0300

    feat: base + descrição da funcionalidade

---
+++ projects/02-esmodules-internacionalization/src/person/person.js
@@ -0,0 +1,23 @@
+import FormatUtil from '../format-util/format-util.js';
+
+export default class Person {
+  constructor({ id, vehicles, kmTravelled, from, to }) {
+    this.id = id;
+    this.vehicles = vehicles;
+    this.kmTravelled = kmTravelled;
+    this.from = from;
+    this.to = to;
+  }
+
+  formatted(language) {
+    const { formatToDate, formatToKilometer, unionValues } = new FormatUtil(language);
+
+    return {
+      id: Number(this.id),
+      vehicles: unionValues(this.vehicles),
+      kmTravelled: formatToKilometer(this.kmTravelled),
+      from: formatToDate(this.from),
+      to: formatToDate(this.to),
+    };
+  }
+}
